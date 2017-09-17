4e5228db8c175c26056abe0080583550f4b233d5
JamesMatchett <30292929+JamesMatchett@users.noreply.github.com>
2017-09-17 11:48:57 +0100

    Add files via upload

---
+++ Confusion Basic Concepts 17-09-17.py
@@ -0,0 +1,29 @@
+
+#You asked "Why does this: print('print("print")')
+#Become this:              print("print")
+
+
+print('print("print")')
+
+#Okay this works above as it will output anything between the outer speech marks
+#because I could even type
+
+print("print('print')")
+#which switches the speech marks to apostaphes (and vice versa) 
+
+#and it will output the same thing, it just always takes it from the outer
+#speechmarks or apostraphes
+
+
+
+name1 = "James"
+name2 = "matchett"
+
+print(name1 + " " + name2)
+
+#above is also a way to do concatenations
+
+
+
+
+
