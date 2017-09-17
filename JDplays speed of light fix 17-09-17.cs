b9bfe46a0e210741ee0cf643c01d5866511b570f
JamesMatchett <30292929+JamesMatchett@users.noreply.github.com>
2017-09-17 15:20:47 +0100

    Add files via upload

---
+++ JDplays speed of light fix 17-09-17.cs
@@ -0,0 +1,32 @@
+﻿
+
+//original code
+if (comboBox3.SelectedItem.ToString() == "Distance" && comboBox2.SelectedItem.ToString() == "Speed of light")
+{
+var3 = textBox3.Text;
+
+        distance = Convert.ToDecimal(var1);
+          
+        time = distance / speedOfLight;
+        var3 = time.ToString();
+        label4.Text = var3;
+        
+    }
+
+
+    //my suggested fix
+
+    if (comboBox3.SelectedItem.ToString() == "Distance" && comboBox2.SelectedItem.ToString() == "Speed of light")
+{
+//remove var3 = textBox3.text as it's not used in this block of code 
+//var3 = textBox3.Text;
+
+
+//var1 isn't given a number in this block, so replace "textbox2.text" below with the correct box
+        distance = Convert.ToDecimal(textbox2.text);
+          
+        time = distance / speedOfLight;
+        var3 = time.ToString();
+        label4.Text = var3;
+        
+    }
