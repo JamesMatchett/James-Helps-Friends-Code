2cbda46515659b513731c8343389f070a7e11873
JamesMatchett <30292929+JamesMatchett@users.noreply.github.com>
2017-09-17 18:39:49 +0100

    ThinkSpider650: Issue #4: Python: hello world

---
+++ ThinkSpider650 Issue 4 PythonHello World.py
@@ -0,0 +1,29 @@
+#Okay First things first you need to download the IDLE for python which will let you make your code
+
+#https://www.python.org/ftp/python/3.6.2/python-3.6.2.exe
+#Just click here and it should download and install
+#Once it's installed search for IDLE on your pc
+#Once that starts click File the New and a second window should appear
+#Once it appears this is the window you do your programming in, we're ready to start!
+
+#For a simple hello world program we need to tell the computer 2 things
+#1: what is has to do and 2: what data it does the action with
+#so in our case the program has to say something
+#and that "something" is hello world"
+
+#in python the way we get the program to output something is to use the word print
+
+print("Hello world")
+
+#using this above, anything inbetween the speech marks is output
+
+#we can even get it to store words and output them together
+
+name = "James"
+surname = "Matchett"
+
+print(name+" "+surname)
+
+#press F5 to run this program, but you have to save it before it runs!
+
+#if you need any help just raise another issue!
